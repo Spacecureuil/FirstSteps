11d4e350b439f28602885db1616846e75f4cfcdc
Pierre Maury <pmaury@c2r6p3.42nice.fr>
2021-01-19 16:31:50 +0100

    Update

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: pmaury <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 13:54:38 by pmaury            #+#    #+#             */
-/*   Updated: 2021/01/18 08:37:31 by pmaury           ###   ########.fr       */
+/*   Updated: 2021/01/19 10:22:52 by pmaury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	ft_rev_int_tab(int *tab, int size)
 
 	i = 0;
 	rev_i = size - 1;
-	while (i < size - 1)
+	while (i < (size / 2))
 	{
 		temp = tab[i];
-		tab[i] = tab[i];
-		tab[i] = temp;
+		tab[i] = tab[rev_i];
+		tab[rev_i] = temp;
 		i++;
-		ri--;
+		rev_i--;
 	}
 }
