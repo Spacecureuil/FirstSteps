66eb41115a963767dd8672d4f00aa018866fb8a2
Pierre Maury <pmaury@c2r10p3.42nice.fr>
2021-01-23 16:52:51 +0100

    Update_C03

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,28 +6,57 @@
 /*   By: pmaury <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/20 09:47:17 by pmaury            #+#    #+#             */
-/*   Updated: 2021/01/21 11:15:02 by pmaury           ###   ########.fr       */
+/*   Updated: 2021/01/23 16:51:17 by pmaury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int		ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < (n - 1) && s1[i] == s2[i] && s1[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
 
 char	*ft_strstr(char *str, char *to_find)
 {
 	int i;
-	int j;
+	int len;
 
 	i = 0;
-	while (str[i] != '\0')
+	len = ft_strlen(to_find);
+	while (str[i])
 	{
-		j = 0;
-		while (to_find[j] == str[i + j])
-		{
-			if (to_find[j + 1] == '\0')
-			{
-				return (str + i);
-			}
-			j++;
-		}
+		if (ft_strncmp(str + i, to_find, len) == 0)
+			return (str + i);
 		i++;
+	}
+	return (0);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int		main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		printf("%s \n", ft_strstr(av[1], av[2]));
+		printf("%s \n", strstr(av[1], av[2]));
 	}
 	return (0);
 }
