f4752f8bfeacbd869284affd2b8d2a199f4794ec
Spacecureuil <p.maury.skj@gmail.com>
2021-01-26 21:49:36 +0100

    C07_Update

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -14,4 +14,29 @@
 
 int	*ft_range(int min, int max)
 {
-	
+	int *tab;
+	int i;
+
+	if (min >= max)
+		return (NULL);
+	tab = (int*)malloc(sizeof(*tab) * (max - min));
+	i = 0;
+	while (min < max)
+	{
+		tab[i] = min;
+		i++;
+		min++;
+	}
+	return (tab);
+}
+
+#include <stdio.h>
+
+int		main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		printf("%d", //toussa toussa faut finir );
+		return (0);
+	}
+}
---
+++ C07/ex03/ft_strjoin.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (0);
+}
+
+char	*ft_create_str(int size, char **strs, char *sep)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	len = 0;
+	i = 0;
+	while (i < size)
+	{
+		len += ft_strlen(strs[i]);
+		i++;
+	}
+	len += ft_strlen(sep) * size - 1;
+	if (size <= 0)
+		len = 1;
+	if (!(str = (char *)malloc(sizeof(char) * len)))
+		return (0);
+	return (str);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	*str;
+
+	str = ft_create_str(size, strs, sep);
+	i = -1;
+	k = 0;
+	while (++i < size)
+	{
+		j = 0;
+		while (strs[i][j])
+		{
+			str[k++] = strs[i][j];
+			j++;
+		}
+		j = 0;
+		while (sep[j] && i != size - 1)
+		{
+			str[k++] = sep[j];
+			j++;
+		}
+	}
+	str[k] = '\0';
+	return (str);
+}
+
+#include <stdio.h>
+
+int		main(int ac, char **av)
+{
+	if (ac == 4)
+		printf("%s\n", ft_strjoin(-1, av, ""));
+}
