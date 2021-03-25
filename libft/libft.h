9ba8e57f07344e7326a09407ee8b4407990e98cf
Pierre Maury <pmaury@c3r2p1.42nice.fr>
2021-03-25 17:11:02 +0100

    libft

---
+++ libft/libft.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmaury <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/25 14:20:25 by pmaury            #+#    #+#             */
+/*   Updated: 2021/03/25 14:21:52 by pmaury           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int ft_atoi(char *str);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+void    ft_putchar_fd(char c, int fd);
+void    ft_putendl_fd(char *s, int fd);
+void    ft_putnbr_fd(int n, int fd);
+void    ft_putstr_fd(char *s, int fd);
+size_t	ft_strlen(const char *str);
+char *ft_tolower(char *str);
+char *ft_toupper(char *str);
+
+#endif
