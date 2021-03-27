95f3af506f1dd191f5ae5a6bce7ef3eacf050893
Spacecureuil <p.maury.skj@gmail.com>
2021-03-27 14:33:10 +0100

    libft

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -10,11 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalnum(int c)
+#include "libft.h"
+
+int		ft_isalnum(int c)
 {
-	if ((c >= 'a' && c <= 'z') ||
-		(c >= 'A' && c <= 'Z') ||
-		(c >= '0' && c <= '9'))
+	if (ft_isalpha(c) || ft_isdigit(c))
 		return (1);
 	return (0);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -13,20 +13,25 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
 
 int ft_atoi(char *str);
+
 int	ft_isalnum(int c);
 int	ft_isalpha(int c);
 int	ft_isascii(int c);
 int	ft_isdigit(int c);
 int	ft_isprint(int c);
+char *ft_tolower(char *str);
+char *ft_toupper(char *str);
+
 void    ft_putchar_fd(char c, int fd);
 void    ft_putendl_fd(char *s, int fd);
 void    ft_putnbr_fd(int n, int fd);
 void    ft_putstr_fd(char *s, int fd);
 size_t	ft_strlen(const char *str);
-char *ft_tolower(char *str);
-char *ft_toupper(char *str);
+
 
 #endif
