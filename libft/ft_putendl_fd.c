92ed552d5fb0d81ab7be9a29d5e21c9cf3fe84c2
Spacecureuil <p.maury.skj@gmail.com>
2021-03-27 14:34:50 +0100

    libft

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -19,7 +19,7 @@ void    ft_putendl_fd(char *s, int fd)
     i = 0;
     while (s[i])
     {
-        ft_putchar_fd(s[i]);
+        ft_putchar_fd(s[i], fd);
         i++;
     }
     write(fd, "\n", 1);
