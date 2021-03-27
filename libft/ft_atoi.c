40c63d63618ba0a7b52bd79761108a93914d9f07
Pierre Maury <pmaury@c3r2p1.42nice.fr>
2021-03-27 15:07:46 +0100

    libft

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -22,9 +22,10 @@ int ft_atoi(char *str)
     while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 			str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
         i++;
-    if (str[i] == '-')
+    if (str[i] == '-' && str[i] == '+')
     {
-        neg *= -1;
+        if (str[i] == '-')
+            neg = 1;
         i++;
     }
     while (str[i] >= '0' && str[i] <= '9')
